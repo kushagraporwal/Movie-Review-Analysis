9a3c86b2beb69937db80c7750d75024e2b2f01b5
Kushagra <kushagraporwal2001@gmail.com>
2021-11-08 11:11:37 +0530

    Project Commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template
+import joblib
+
+
+app = Flask(__name__)
+model = joblib.load(open('model.pkl', 'rb'))
+cv = joblib.load(open('cv.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+
+    if request.method == 'POST':
+        text = request.form['Review']
+        data = [text]
+        vectorizer = cv.transform(data).toarray()
+        prediction = model.predict(vectorizer)
+    if prediction:
+        return render_template('index.html', prediction_text='Positive Review  ')
+    else:
+        return render_template('index.html', prediction_text='Negative Review  ')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
